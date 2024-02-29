4e7785f10447de0495810108288c02d6bcfc006f
Enrico Tagliaferri <errotagliaferri.4801@gmail.com>
2024-02-29 10:02:44 +0100

    Create beforeEach method to initialize list

--- tdd-exercise/src/test/java/filter/FilteredCircularListTest.java
+++ tdd-exercise/src/test/java/filter/FilteredCircularListTest.java
@@ -2,13 +2,20 @@ package filter;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class FilteredCircularListTest {
+
+    FilteredCircularList list;
+
+    @BeforeEach
+    void beforeEach() {
+        list = new SimpleFilteredCircularList();
+    }
     
     @Test
     void initialize() {
-        FilteredCircularList list = new SimpleFilteredCircularList();
         assertTrue(list.isEmpty());
     }
 }
